986ad56a6821a5cc36dc5e71fad4c9310ad5f2a1
souts jjw <souts.jjw@gmail.com>
2021-11-06 00:34:57 +0800

    [Day14] Flutter with GetX animated_text_kit
    https://ithelp.ithome.com.tw/articles/10273755

--- lib/pages/cached_network_image/cached_network_image_page.dart
+++ lib/pages/cached_network_image/cached_network_image_page.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_ithome/utils/image_cached.dart';
 import 'package:get/get.dart';
@@ -13,11 +14,42 @@ class CachedNetworkImagePage extends GetView<CachedNetworkImageController> {
         child: SizedBox.expand(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Expanded(
+            children: [
+              const Expanded(
                 child: ImageCached(
                   imageUrl:
                       '/assets/uploads/media-uploader/flutter-app-development-company1623499975.jpg',
+                ),
+              ),
+              Expanded(
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Hello world!',
+                      textStyle: const TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      speed: const Duration(milliseconds: 500),
+                    ),
+                  ],
+                  totalRepeatCount: 4,
+                ),
+              ),
+              Expanded(
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    FadeAnimatedText(
+                      'Fade First',
+                      textStyle: const TextStyle(
+                          fontSize: 32.0, fontWeight: FontWeight.bold),
+                    ),
+                    ScaleAnimatedText(
+                      'Then Scale',
+                      textStyle: const TextStyle(
+                          fontSize: 70.0, fontFamily: 'Canterbury'),
+                    ),
+                  ],
                 ),
               ),
             ],
